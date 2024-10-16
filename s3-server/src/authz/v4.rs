07f8e5f153068c51fe16d98d40007a32abb432c3
Aaron Lee <sphinxer34@yahoo.com>
2024-10-15 22:01:17 -0700

    Add more tests

--- s3-server/src/authz/v4.rs
+++ s3-server/src/authz/v4.rs
@@ -68,14 +68,48 @@ fn get_payload_hash(req: &Request) -> String {
     UNSIGNED_HASH.to_string()
 }
 
+fn url_encode(s: &str, encode_slash: bool) -> String {
+    fn is_unreserved(c: char, encode_slash: bool) -> bool {
+        c.is_ascii_alphanumeric()
+            || matches!(c, '-' | '_' | '.' | '~')
+            || (c == '/' && !encode_slash)
+    }
+
+    s.chars()
+        .map(|c| {
+            if is_unreserved(c, encode_slash) {
+                c.to_string()
+            } else {
+                format!("%{:02X}", c as u8)
+            }
+        })
+        .collect::<Vec<String>>()
+        .join("")
+}
+
 fn get_canonical_request(req: &Request) -> String {
     let method = req.method().as_str();
 
-    // TODO: URI encode
-    let uri = req.uri().path_and_query().unwrap().as_str();
+    let uri = url_encode(req.uri().path(), false);
 
-    // TODO: URI encode
-    let query = req.uri().query().unwrap_or("");
+    let mut query = req.uri().query().unwrap_or("").to_string();
+    if !query.is_empty() {
+        let mut query_params = query
+            .split('&')
+            .map(|param| {
+                let mut parts = param.split('=');
+                let key = parts.next().unwrap();
+                let val = parts.next().unwrap_or("");
+                (url_encode(key, true), url_encode(val, true))
+            })
+            .collect::<Vec<_>>();
+        query_params.sort_unstable();
+        query = query_params
+            .iter()
+            .map(|(k, v)| format!("{}={}", k, v))
+            .collect::<Vec<String>>()
+            .join("&");
+    }
 
     let mut headers = get_signed_headers(req)
         .iter()
@@ -138,9 +172,9 @@ pub fn does_signature_match_v4(
         .eq(&0)
     {
         tracing::debug!(
-            canonical_request = canonical_request.as_str(),
+            canonical_request = canonical_request,
             string_to_sign = string_to_sign,
-            signature = signature.as_str(),
+            signature = signature,
             auth_signature = auth_signature,
             "Signature does not match"
         );
@@ -186,30 +220,7 @@ mod tests {
     }
 
     #[test]
-    fn test_string_to_sign() {
-        let req = Request::builder()
-            .method("GET")
-            .header("Host", "127.0.0.1:3000")
-            .header("X-Amz-Content-Sha256", UNSIGNED_HASH)
-            .header("X-Amz-Date", "20241016T040941Z")
-            .uri("/")
-            .body(axum::body::Body::empty())
-            .unwrap();
-        let canonical_request = get_canonical_request(&req);
-
-        let string_to_sign = get_string_to_sign(
-            chrono::NaiveDateTime::parse_from_str("20241016T040941Z", "%Y%m%dT%H%M%SZ").unwrap(),
-            "us-east-1",
-            &canonical_request,
-        );
-        assert_eq!(
-            string_to_sign,
-            "AWS4-HMAC-SHA256\n20241016T040941Z\n20241016/us-east-1/s3/aws4_request\n7fea3df265d1b35a65ecb5a310e41caaeab9886b18ef4db00eb1f65dc06d3c4f"
-        );
-    }
-
-    #[test]
-    fn test_get_signature() {
+    fn test_get_object() {
         let signing_key = get_signing_key(
             SECRET_KEY,
             chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
@@ -222,6 +233,122 @@ mod tests {
         assert_eq!(
             signature,
             "f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41"
+        );
+    }
+
+    #[test]
+    fn test_put_object() {
+        let req = Request::builder()
+            .method("PUT")
+            .header("Host", "examplebucket.s3.amazonaws.com")
+            .header(
+                "x-amz-content-sha256",
+                "44ce7dd67c959e0d3524ffac1771dfbba87d2b6b4b4e99e42034a8b803f8b072",
+            )
+            .header("Date", "Fri, 24 May 2013 00:00:00 GMT")
+            .header("x-amz-date", "20130524T000000Z")
+            .header("x-amz-storage-class", "REDUCED_REDUNDANCY")
+            .uri("/test$file.text")
+            .body(axum::body::Body::from("Welcome to Amazon S3."))
+            .unwrap();
+
+        let canonical_request = get_canonical_request(&req);
+        assert_eq!(
+            canonical_request,
+            "PUT\n/test%24file.text\n\ndate:Fri, 24 May 2013 00:00:00 GMT\nhost:examplebucket.s3.amazonaws.com\nx-amz-content-sha256:44ce7dd67c959e0d3524ffac1771dfbba87d2b6b4b4e99e42034a8b803f8b072\nx-amz-date:20130524T000000Z\nx-amz-storage-class:REDUCED_REDUNDANCY\n\ndate;host;x-amz-content-sha256;x-amz-date;x-amz-storage-class\n44ce7dd67c959e0d3524ffac1771dfbba87d2b6b4b4e99e42034a8b803f8b072"
+        );
+
+        let string_to_sign = get_string_to_sign(
+            chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
+            "us-east-1",
+            &canonical_request,
+        );
+
+        let signing_key = get_signing_key(
+            SECRET_KEY,
+            chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
+            "us-east-1",
+        );
+
+        let signature = get_signature(signing_key, &string_to_sign);
+
+        assert_eq!(
+            signature,
+            "98ad721746da40c64f1a55b78f14c238d841ea1380cd77a1b5971af0ece108bd"
+        );
+    }
+
+    #[test]
+    fn test_get_bucket_lifecycle() {
+        let req = Request::builder()
+            .method("GET")
+            .header("Host", "examplebucket.s3.amazonaws.com")
+            .header("x-amz-content-sha256", UNSIGNED_HASH)
+            .header("x-amz-date", "20130524T000000Z")
+            .uri("/?lifecycle")
+            .body(axum::body::Body::empty())
+            .unwrap();
+
+        let canonical_request = get_canonical_request(&req);
+        assert_eq!(
+            canonical_request,
+            "GET\n/\nlifecycle=\nhost:examplebucket.s3.amazonaws.com\nx-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\nx-amz-date:20130524T000000Z\n\nhost;x-amz-content-sha256;x-amz-date\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        );
+
+        let string_to_sign = get_string_to_sign(
+            chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
+            "us-east-1",
+            &canonical_request,
+        );
+
+        let signing_key = get_signing_key(
+            SECRET_KEY,
+            chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
+            "us-east-1",
+        );
+
+        let signature = get_signature(signing_key, &string_to_sign);
+
+        assert_eq!(
+            signature,
+            "fea454ca298b7da1c68078a5d1bdbfbbe0d65c699e0f91ac7a200a0136783543"
+        );
+    }
+
+    #[test]
+    fn test_list_objects() {
+        let req = Request::builder()
+            .method("GET")
+            .header("Host", "examplebucket.s3.amazonaws.com")
+            .header("x-amz-content-sha256", UNSIGNED_HASH)
+            .header("x-amz-date", "20130524T000000Z")
+            .uri("/?max-keys=2&prefix=J")
+            .body(axum::body::Body::empty())
+            .unwrap();
+
+        let canonical_request = get_canonical_request(&req);
+        assert_eq!(
+            canonical_request,
+            "GET\n/\nmax-keys=2&prefix=J\nhost:examplebucket.s3.amazonaws.com\nx-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\nx-amz-date:20130524T000000Z\n\nhost;x-amz-content-sha256;x-amz-date\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        );
+
+        let string_to_sign = get_string_to_sign(
+            chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
+            "us-east-1",
+            &canonical_request,
+        );
+
+        let signing_key = get_signing_key(
+            SECRET_KEY,
+            chrono::NaiveDateTime::parse_from_str("20130524T000000Z", "%Y%m%dT%H%M%SZ").unwrap(),
+            "us-east-1",
+        );
+
+        let signature = get_signature(signing_key, &string_to_sign);
+
+        assert_eq!(
+            signature,
+            "34b48302e7b5fa45bde8084f4b7868a86f0a534bc59db6670ed5711ef69dc6f7"
         );
     }
 }
